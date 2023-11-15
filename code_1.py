32bff9501261e7354938f77e1595910510e6af9e
jabi47657 <146475003+jabi47657@users.noreply.github.com>
2023-11-15 11:28:26 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,14 @@
-def main():
-    return data
-def generate_random_data():
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
 import random
 
-    for item in data:
-    main()
     data = generate_random_data()
-
+    return data
 if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
         print(f"Random Number: {item}")
+def main():
+    main()
+    for item in data:
+
+def generate_random_data():
