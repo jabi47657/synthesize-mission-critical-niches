bbc8ced469e05d185e2ae92f63a303c4a0ba3a35
jabi47657 <146475003+jabi47657@users.noreply.github.com>
2023-11-15 08:09:36 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+    const data = generateRandomData();
+main();
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+function generateRandomData() {
+}
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+
+    return data;
+        console.log("Random Number: " + item);
+    });
+    }
+}
+function main() {
+    data.forEach(item => {
