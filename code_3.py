5d74b750403ce9598ddc084bef078be6004dfa81
jabi47657 <146475003+jabi47657@users.noreply.github.com>
2023-11-15 11:50:29 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,26 +1,43 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+function generateRandomData() {
+    });
+}
+    const data = generateRandomData();
+function main() {
+    for (let i = 0; i < 10; i++) {
+    }
 
+        console.log("Random Number: " + item);
+
+    return data;
+
+}
+    data.forEach(item => {
+main();
+import random
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    main()
 def main():
 
-if __name__ == "__main__":
-def generate_random_data():
-
-
-    data = generate_random_data()
-import random
     return data
-        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
-    main()
     for item in data:
 
 import random
-    main()
-    for item in data:
-        print(f"Random Number: {item}")
 
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    return data
-if __name__ == "__main__":
+    main()
     data = generate_random_data()
+def main():
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
