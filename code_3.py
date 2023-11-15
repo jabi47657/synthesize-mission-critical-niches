164c6f3cc6b29acc0e772e21d65220495a1530fa
jabi47657 <146475003+jabi47657@users.noreply.github.com>
2023-11-15 10:00:10 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,69 +1,26 @@
-    return data
 
-    main()
+def generate_random_data():
 
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    for item in data:
+    main()
+if __name__ == "__main__":
+    return data
         print(f"Random Number: {item}")
 
-if __name__ == "__main__":
+
+def main():
     data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:        }
+        int[] data = new int[10];
+        }
 
-def main():}
-    for (let i = 0; i < 10; i++) {
 
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-    return data;
-    const data = [];
-function generateRandomData() {
-main();
-
-    const data = generateRandomData();
-}
-    data.forEach(item => {
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        for (int item : data) {
     }
-function main() {
-    });
-        console.log("Random Number: " + item);
-    for item in data:
-
-        print(f"Random Number: {item}")
-def main():
-    data = generate_random_data()
-def generate_random_data():
-
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-    return data
-if __name__ == "__main__":
-    for item in data:
-    data = generate_random_data()
-
-def main():
-if __name__ == "__main__":
-
-
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-def generate_random_data():
-
-        print(f"Random Number: {item}")
-    return data
-        print(f"Random Number: {item}")
-    for item in data:
-
-    data = generate_random_data()
-    main()
-
-
-import random
-def main():
-    return data
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+}
+            System.out.println("Random Number: " + item);
